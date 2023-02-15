de7de8744d78a55df3c6ce48edeb68cca319f5ca
Nadir <yusipov.nadir@gmail.com>
2023-02-15 13:59:00 +0300

    добавил пользователя в админку

--- core/models.py
+++ core/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
